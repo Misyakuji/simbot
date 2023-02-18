02850fc36cff59307802e891dbacc05f922eac83
YaeMiko <2810013347@qq.com>
2023-02-18 19:11:20 +0800

    init

---
+++ simbot-starter/src/main/java/com/miko/StartApplication.java
@@ -0,0 +1,18 @@
+package com.miko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * [描述]
+ *
+ * @author DengXueQian
+ * @version v1.0
+ * @createTime 2023/2/18 15:07
+ */
+@SpringBootApplication
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
