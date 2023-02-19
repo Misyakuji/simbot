5f150483f1b802fca901fa904fe09a0fe9e859ba
YaeMiko <2810013347@qq.com>
2023-02-19 17:49:11 +0800

    Constructed module

--- simbot-robot/src/main/java/com/miko/BotApplication.java
+++ simbot-robot/src/main/java/com/miko/BotApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableSimbot
 @SpringBootApplication
-public class StartApplication {
+public class BotApplication {
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
     }
