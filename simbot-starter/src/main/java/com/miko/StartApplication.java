be7e7a4597c58893c9d95eaae2af4e68cddf1e57
shixiao <65637746+koksk-kcss@users.noreply.github.com>
2024-02-01 11:23:53 +0800

    Changes

--- simbot-starter/src/main/java/com/miko/StartApplication.java
+++ simbot-starter/src/main/java/com/miko/StartApplication.java
@@ -1,6 +1,6 @@
 package com.miko;
 
-import love.forte.simboot.spring.autoconfigure.EnableSimbot;
+//import love.forte.simboot.spring.autoconfigure.EnableSimbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BotApplication.class, args);
+        SpringApplication.run(StartApplication.class, args);
     }
 }
